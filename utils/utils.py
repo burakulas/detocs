581b946d0a638c1df3ca350158b1ad9500edf73b
burakulas <35465277+burakulas@users.noreply.github.com>
2024-08-02 08:27:15 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -73,7 +73,7 @@ def detect_objects(image,model_op,conf_th):
     if model_op == "SSD":
       interpreter = tf.lite.Interpreter(model_path="models/ssd/detect.tflite")
     elif model_op == "Eff":
-      interpreter = tf.lite.Interpreter(model_path="models/effdet/eff_detect.tflite")
+      interpreter = tf.lite.Interpreter(model_path="models/effdet/Eff_detect.tflite")
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
